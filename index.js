1342a4aede8dd1036a61432e003ec359ff95e0fe
Michał Budzyński <michal@virtualdesign.pl>
2014-11-19 01:01:00 +0100

    :stew: links filtered properly

--- index.js
+++ index.js
@@ -9,7 +9,16 @@ var gse = function(title, cb) {
   request(url+title, function (error, response, body) {
     if (!error && response.statusCode == 200) {
       var results = body.match(/\/fileinfo\/gs([\w]+)/gi);
-      console.log(results);
+      if (results) {
+        var filteredResults;
+        filteredResults = results.filter(function(item, pos, self) {
+          return self.indexOf(item) == pos;
+        });
+
+        console.log(filteredResults);
+      } else {
+        // no results
+      }
     }
   });
 };
@@ -18,3 +27,5 @@ var gse = function(title, cb) {
 gse('American Dad! S01E01', function(err, result) {
 
 });
+
+//GENERALFIL.ES
