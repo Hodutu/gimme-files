06d6455772aaf092a445555a84f9797d275c4619
Michał Budzyński <michal@virtualdesign.pl>
2014-11-29 01:01:00 +0100

    :carousel_horse: general-search to general-files

--- index.js
+++ index.js
@@ -2,23 +2,18 @@
 
 var request = require('request');
 
-var url = 'http://www.general-search.com/download/';
+var url = 'http://www.generalfil.es/files-a/';
+var options = '/?filter=_Video_';
 
 var gse = function(title, cb) {
   title = title.replace(/[^(\w)]+/gi, '-').toLowerCase();
-  request(url+title, function (error, response, body) {
+  request(url+title+options, function (error, response, body) {
     if (!error && response.statusCode == 200) {
-      var results = body.match(/\/fileinfo\/gs([\w]+)/gi);
-      if (results) {
-        var filteredResults;
-        filteredResults = results.filter(function(item, pos, self) {
-          return self.indexOf(item) == pos;
-        });
-
-        console.log(filteredResults);
-      } else {
-        // no results
-      }
+      var links = body.match(/gf_ShowDownloadLink\([\w',?=\/&]+/gi);
+      links = links.map(function(link){
+        return link.split(',').pop().replace(/'/gi, '');
+      });
+      console.log(links);
     }
   });
 };
